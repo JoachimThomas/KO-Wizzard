56d6b4c1db40061440d59643d0033b0df1c63c96
Joachim <jewtis@gmail.com>
2025-11-14 04:16:50 +0100

    Initial Commit

---
+++ KO-Wizzard/KO_WizzardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KO_WizzardApp.swift
+//  KO-Wizzard
+//
+//  Created by Joachim Thomas on 14.11.25.
+//
+
+import SwiftUI
+
+@main
+struct KO_WizzardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
